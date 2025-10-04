16914d663c468567e9a4e08141598401bbe3a69f
ehileenfernandez <ehileenfernandez@gmail.com>
2025-10-03 21:50:16 -0500

    ubicación de archivo corregido
    valida los datos cuando el usuario envía el formulario y si todo es válido crea un nuevo objeto usuario, lo añade al arreglo users_base y guarda ese arreglo en localStorage bajo la clave 'repo_json/users.json'.

---
+++ front/js/register.js
@@ -0,0 +1,194 @@
+import { updateUsers } from "./main.js"
+
+let new_user_registration = document.getElementById('main-register-user')
+let cancel_new_user_button = document.getElementsByClassName('cancel-new-user')[0]
+const fnameError = document.getElementById('fname-error')
+const lnameError = document.getElementById('lname-error')
+const addressError = document.getElementById('address-error')
+const usernameError = document.getElementById('username-error')
+const documentidError = document.getElementById('documentid-error')
+const newPasswordError = document.getElementById('new-password-error')
+const confirmNewPasswordError = document.getElementById('confirm-new-password-error')
+
+export function new_register(){   
+    new_user_registration.style.display = "block"
+}
+let users_base = JSON.parse(localStorage.getItem('repo_json/users.json'))
+
+class NewUserRegister {
+    
+    constructor(fname,lname,username,documentid,new_password,address){
+        this.fname = fname
+        this.lname = lname
+        this.username = username
+        this.documentid = documentid
+        this.newPassword = new_password      
+        this.address = address
+
+        this.createNewUser()
+    }
+    createNewUser() {
+        let nextUserID
+        if(users_base.length==0){
+            nextUserID =1
+        } else {
+            nextUserID = Math.max(...users_base.map(user =>user.user_id))+1
+        }
+        
+        const user_new_register = {user_id:nextUserID,user_username:this.username,
+            user_fname:this.fname,user_lname:this.lname,user_address:this.address,
+            user_documentid:this.documentid,user_password:this.newPassword}
+        //alert(JSON.stringify(user_new_register, null, 2));
+        users_base.push(user_new_register)
+        localStorage.setItem('repo_json/users.json', JSON.stringify(users_base));
+    }
+}
+
+if (window.location.pathname.includes('index.html')) {
+    let new_user_registration = document.getElementById('main-register-user');
+    let cancel_new_user_button = document.getElementsByClassName('cancel-new-user')[0];
+    
+    if (cancel_new_user_button) {
+        cancel_new_user_button.onclick = function () {
+            new_user_registration.style.display = "none";
+            document.getElementById('new-user-data').reset();
+            fnameError.style.display = 'none';
+            lnameError.style.display = 'none';
+            addressError.style.display = 'none';
+            usernameError.style.display = 'none';
+            documentidError.style.display = 'none';
+            newPasswordError.style.display = 'none';
+            confirmNewPasswordError.style.display = 'none';
+        };
+    }
+}
+
+function existingUser(userNumberId='',userUsername=''){
+    if (!userUsername){
+        return users_base.some(record => record.user_documentid.toLowerCase()==userNumberId.toLowerCase())
+    } else if (!userNumberId){
+        return users_base.some(record => record.user_username.toLowerCase()==userUsername.toLowerCase())
+    }
+}
+
+function validateFields(event){
+    event.preventDefault()
+    let formValid = true    
+    const fname = document.getElementById('fname')
+    const lname = document.getElementById('lname')
+    const address = document.getElementById('address')
+    const username = document.getElementById('username')
+    const documentid = document.getElementById('documentid')
+    const newPassword = document.getElementById('new-password')
+    const confirmNewPassword = document.getElementById('confirm-new-password')
+        
+    if (!fname.value) {
+        fnameError.textContent = 'Por favor, ingresa tu nombre.';
+        fnameError.style.display = 'block';
+        formValid = false;
+
+    } else if (fname.value.length<2){
+        fnameError.textContent = `El nombre ${fname.value},es muy corto, por favor ingrese un nombre valido`;
+        fnameError.style.display = 'block';
+        formValid = false;
+    } else {
+        fnameError.style.display = 'none';
+    }
+
+    if (!lname.value) {
+        lnameError.textContent = 'Por favor, ingresa tu apellido.';
+        lnameError.style.display = 'block';
+        formValid = false;
+    } else if (lname.value.length <2 ){
+        lnameError.textContent = `El apellido ${lname.value},es muy corto, por favor ingrese un apellido valido`;
+        lnameError.style.display = 'block';
+        formValid = false;
+    } else {
+        lnameError.style.display = 'none';
+    }
+
+    if (!address.value) {
+        addressError.textContent = 'Por favor, ingresa tu dirección.';
+        addressError.style.display = 'block';
+        formValid = false;
+    } else if (address.value.length <10){
+        addressError.textContent = `La dirección ${address.value},es muy corto, por favor ingrese una dirección valida`;
+        addressError.style.display = 'block';
+        formValid = false;
+    } else {
+        addressError.style.display = 'none';
+    }
+
+    if (!username.value) {
+        usernameError.textContent = 'Por favor, ingresa tu nombre de usuario.';
+        usernameError.style.display = 'block';
+        formValid = false;
+
+    } else if (username.value.length<5){
+        usernameError.textContent = `El nombre de usuario debe contener al menos 5 caracteres`;
+        usernameError.style.display = 'block';
+        formValid = false;
+    } else if (existingUser(undefined,username.value)){
+        usernameError.textContent = `El nombre de usuario ya existe, use su nombre de usuario para iniciar sesion`;
+        usernameError.style.display = 'block';
+        formValid = false;
+    } else {
+        usernameError.style.display = 'none';
+    }
+
+    if (!documentid.value) {
+        documentidError.textContent = 'Por favor, ingresa tu numero de documento.';
+        documentidError.style.display = 'block';
+        formValid = false;
+    } else if(documentid.value.length<7){
+        documentidError.textContent = `El numero de identicacion solo tiene ${documentid.value.length} caracteres, el minimo es 7`;
+        documentidError.style.display = 'block';
+        formValid = false;
+    } else if(existingUser(documentid.value)){
+        documentidError.textContent = `El numero de identicacion ya existe, use su nombre de usuario para iniciar sesion`;
+        documentidError.style.display = 'block';
+        formValid = false;
+    } else {
+        documentidError.style.display = 'none';
+    }
+
+    if (!newPassword.value) {
+        newPasswordError.textContent = 'Por favor, ingresa tu contraseña.';
+        newPasswordError.style.display = 'block';
+        formValid = false;
+    } else if(newPassword.value.length<4){
+        newPasswordError.textContent = `Su contraseña solo tiene ${newPassword.value.length} caracteres, el minimo es 4`;
+        newPasswordError.style.display = 'block';
+        formValid = false;
+    } else {
+        newPasswordError.style.display = 'none';
+    }
+
+    if (!confirmNewPassword.value) {
+        confirmNewPasswordError.textContent = 'Por favor, confirma tu contraseña.';
+        confirmNewPasswordError.style.display = 'block';
+        formValid = false;
+    } else {
+        confirmNewPasswordError.style.display = 'none';
+    }
+
+    if(newPassword.value!=confirmNewPassword.value){
+        confirmNewPasswordError.textContent = 'Por favor, intenta de nuevo, contraseña no coinciden.';
+        confirmNewPasswordError.style.display = 'block';        
+        formValid = false;
+        newPassword.value =''
+        confirmNewPassword.value=''
+    } else {
+        confirmNewPasswordError.style.display = 'none';
+    }
+
+    if (formValid){
+        const new_user_register = new NewUserRegister(fname.value,
+            lname.value,username.value,documentid.value,newPassword.value,address.value
+        )
+        new_user_registration.style.display = "none";
+        updateUsers()
+    }
+}
+
+document.getElementById('new-user-data').addEventListener('submit',validateFields)
